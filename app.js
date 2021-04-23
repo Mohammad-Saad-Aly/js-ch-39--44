66e98a73cc5506d463b46687bf101efa532d4056
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2021-04-23 16:26:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+// var a = +prompt("Enter kardo value");
+// var a = 0
+// switch (a) {
+//     case 0:
+//     alert("day is sunday")
+//     break;
+//     case 1: case 3:
+//         document.write("zero he ye");
+//         break;
+
+// default:
+// alert("acha sahi he")
+// case 1:
+//     document.write("one he ye");
+//     break;
+
+// case 2:
+//     document.write("two he ye");
+//     break;
+
+// case 3:
+//     document.write("three he ye");
+//     break;
+
+// }
+
+
+// var i = 0;
+// while(i <= 10){
+//     document.write(i,"<br>")
+//     i++
+// }
+
+//var i = 4;
+
+// do {
+//     document.write(i);
+//     i++;
+// } while (i <= 3);
+
+
+// function add(a,b){
+//     return a+b
+// }
+// document.write(add(2,3))
+// document.write(add(10,30))
